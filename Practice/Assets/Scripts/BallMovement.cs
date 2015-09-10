c449ba461b2637e07a9f6c5faad660274bd03d1f
zadieross <zross@bennington.edu>
2015-09-10 15:04:37 -0400

    Ball Movement

---
+++ Practice/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+	private Rigidbody BallBody;
+
+	// Use this for initialization
+	void Start () {
+		BallBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float BallHorizontal = Input.GetAxis ("Horizontal");
+		float BallVertical = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (BallHorizontal, 0, BallVertical);
+
+		BallBody.AddForce (move);
+	}
+}
